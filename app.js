a8c62363e4022b08b5a0f6d2ea67e31e8e05253d
armdedioz <armand.dedios464@gmail.com>
2025-09-20 19:35:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+const clickSound = new Audio();
+const checkedSound = new Audio();
 
+
+
+// sound fx
+clickSound.src = "sounds/click.mp3";
+checkedSound.src = "sounds/xp.mp3";
+checkedSound.volume = 0.6;
 function addTask(){
     if(inputBox.value === ''){
         alert("You must write something!");
@@ -12,6 +20,8 @@ function addTask(){
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        clickSound.currentTime = 0;
+        clickSound.play();
     }
     inputBox.value = "";
      saveData();
@@ -20,10 +30,16 @@ function addTask(){
 listContainer.addEventListener("click",function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        const isChecked = e.target.classList.contains("checked");
+        const eitherSound = isChecked ? checkedSound : clickSound;
+        eitherSound.currentTime = 0;
+        eitherSound.play();
         saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        clickSound.currentTime = 0;
+        clickSound.play();
         saveData();
     }
 }, false);
